5634fdbbd2343fca8cc1702d6c998a679fcacb97
Raul <rrequero@gmail.com>
2022-08-22 19:22:12 +0200

    update install

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,6 @@ DUCKDB_VERSION=0.4.0
 
 mkdir -p /var/lib/libduckdb
 
-	unzip -u /var/lib/libduckdb/libduckdb.zip -d /var/lib/libduckdb
 if [ "$SO" == "Darwin" ]; then
     curl -Lo /var/lib/libduckdb/libduckdb.zip https://github.com/duckdb/duckdb/releases/download/v${DUCKDB_VERSION}/libduckdb-osx-universal.zip
     wget -O /usr/local/bin/geotemporal-data-explorer https://storage.googleapis.com/geotemporal-data-explorer-releases/versions/geotemporal-data-explorer-darwin-arm64-latest
