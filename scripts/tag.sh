8b2d10dc9d5c0eb166d0e6f6e2a437f9269463bd
Raul <rrequero@gmail.com>
2022-08-22 18:59:10 +0200

    first cloudbuild

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,8 +1,8 @@
 echo "Creating release in mac $1"
 make VERSION=$1 release-mac 
 
-gsutil cp ./dist/geotemporal-data-explorer-darwin-arm64-$1 gs://raul-import/versions/$1
-gsutil cp ./dist/geotemporal-data-explorer-darwin-arm64-$1 gs://raul-import/versions/$1/geotemporal-data-explorer-darwin-arm64-latest
+gsutil cp ./dist/geotemporal-data-explorer-darwin-arm64-$1 gs://geotemporal-data-explorer-releases/versions/geotemporal-data-explorer-darwin-arm64-$1
+gsutil cp ./dist/geotemporal-data-explorer-darwin-arm64-$1 gs://geotemporal-data-explorer-releases/versions/geotemporal-data-explorer-darwin-arm64-latest
 
 echo "Creating tag in git"
 git tag $1
